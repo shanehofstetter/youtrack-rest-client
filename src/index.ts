1573ac7c2bcd0b0c1b0f1a9db70b3623d1c877ad
Shane Hofstetter <shane.hofstetter@gmail.com>
2019-03-10 17:15:07 +0100

    update exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from "./youtrack";
 
+export * from "./entities/command";
 export * from "./entities/comment";
 export * from "./entities/customField";
 export * from "./entities/customFieldDefaults";
@@ -7,10 +8,8 @@ export * from "./entities/issue";
 export * from "./entities/issueTag";
 export * from "./entities/project";
 export * from "./entities/projectCustomField";
-export * from "./entities/issueTag";
 export * from "./entities/user";
 export * from "./entities/workItem";
-export * from "./entities/command";
 
 export * from "./endpoints/comment";
 export * from "./endpoints/issue";
